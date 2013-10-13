95e089295f61f1cbbbd1a4755e17571227e642fd
Edvin <ummahusla@gmail.com>
2013-10-13 20:42:56 +0100

    Full working game

--- games/kill-em-all/game.js
+++ games/kill-em-all/game.js
@@ -64,6 +64,7 @@ function randomRange (min, max) {
 }
 
 
+
 function Player() {
     this.srcX = 0;
     this.srcY = 600;
@@ -79,25 +80,25 @@ function Player() {
     this.isDownKey = false;
     this.isLeftKey = false;
     this.isSpacebar = false;
-    //this.isShooting = false;
-    //var numBullets = 10;
-    //this.bullets = [];
-    //this.currentBullet = 0;
-    //for (var i = 0; i < numBullets; i++) {
-        //this.bullets[this.bullets.length] = new Bullet();
-    //}
+    this.isShooting = false;
+    var numBullets = 10;
+    this.bullets = [];
+    this.currentBullet = 0;
+    for (var i = 0; i < numBullets; i++) {
+        this.bullets[this.bullets.length] = new Bullet();
+    }
 }
 
 Player.prototype.update = function () {
     this.centerX = this.drawX + (this.width / 2);
     this.centerY = this.drawY + (this.height / 2);
     this.checkDirection();
-    //this.checkShooting();
-    //this.updateAllBullets();
+    this.checkShooting();
+    this.updateAllBullets();
 };
 
 Player.prototype.draw = function () {
-    //this.drawAllBullets();
+    this.drawAllBullets();
     ctxEntities.drawImage(imgSprite, this.srcX, this.srcY, this.width, this.height, this.drawX, this.drawY, this.width, this.height);
 };
 
@@ -144,9 +145,123 @@ Player.prototype.checkObstacleCollide = function (newDrawX, newDrawY) {
     } else {
         return true;
     }
-
 };
 
+
+
+Player.prototype.checkShooting = function () {
+    if (this.isSpacebar && !this.isShooting) {
+        this.isShooting = true;
+        this.bullets[this.currentBullet].fire(this.centerX, this.centerY);
+        this.currentBullet++;
+        if (this.currentBullet >= this.bullets.length) {
+            this.currentBullet = 0;
+        }
+    } else if (!this.isSpacebar) {
+        this.isShooting = false;
+    }
+};
+
+Player.prototype.updateAllBullets = function () {
+    for (var i = 0; i < this.bullets.length; i++) {
+        if (this.bullets[i].isFlying) {
+            this.bullets[i].update();
+        }
+    }
+};
+
+Player.prototype.drawAllBullets = function () {
+    for (var i = 0; i < this.bullets.length; i++) {
+        if (this.bullets[i].isFlying) {
+            this.bullets[i].draw();
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+
+function Bullet() {
+    this.radius = 2;
+    this.width = this.radius * 2;
+    this.height = this.radius * 2;
+    this.drawX = 0;
+    this.drawY = 0;
+    this.isFlying = false;
+    this.xVel = 0;
+    this.yVel = 0;
+    this.speed = 6;
+}
+
+Bullet.prototype.update = function () {
+    this.drawX += this.xVel;
+    this.drawY += this.yVel;
+    this.checkHitEnemy();
+    this.checkHitObstacle();
+    this.checkOutOfBounds();
+};
+
+Bullet.prototype.draw = function () {
+    ctxEntities.fillStyle = "white";
+    ctxEntities.beginPath();
+    ctxEntities.arc(this.drawX, this.drawY, this.radius, 0, Math.PI * 2, false);
+    ctxEntities.closePath();
+    ctxEntities.fill();
+};
+
+Bullet.prototype.fire = function (startX, startY) {
+    var soundEffect = new Audio("audio/shooting.wav");
+    soundEffect.play();
+    this.drawX = startX;
+    this.drawY = startY;
+    if (player1.srcX === 0) { // Facing south
+        this.xVel = 0;
+        this.yVel = this.speed;
+    } else if (player1.srcX === 35) { // Facing north
+        this.xVel = 0;
+        this.yVel = -this.speed;
+    } else if (player1.srcX === 70) { // Facing west
+        this.xVel = -this.speed;
+        this.yVel = 0;
+    } else if (player1.srcX === 105) { // Facing east
+        this.xVel = this.speed;
+        this.yVel = 0;
+    }
+    this.isFlying = true;
+};
+
+Bullet.prototype.recycle = function () {
+    this.isFlying = false;
+};
+
+Bullet.prototype.checkHitEnemy = function () {
+    for (var i = 0; i < enemies.length; i++) {
+        if (collision(this, enemies[i]) && !enemies[i].isDead) {
+            this.recycle();
+            enemies[i].die();
+        }
+    }
+};
+
+Bullet.prototype.checkHitObstacle = function () {
+    for (var i = 0; i < obstacles.length; i++) {
+        if (collision(this, obstacles[i])) {
+            this.recycle();
+        }
+    }
+};
+
+Bullet.prototype.checkOutOfBounds = function () {
+    if (outOfBounds(this, this.drawX, this.drawY)) {
+        this.recycle();
+    }
+};
 
 
 
@@ -177,6 +292,101 @@ function defineObstacles() {
         new Obstacle(570, 138, 150, bushHeight),
         new Obstacle(605, 492, 90, bushHeight)];
 }
+
+
+
+function Enemy() {
+    this.srcX = 140;
+    this.srcY = 600;
+    this.width = 45;
+    this.height = 54;
+    this.drawX = randomRange(0, canvasWidth - this.width);
+    this.drawY = randomRange(0, canvasHeight - this.height);
+    this.centerX = this.drawX + (this.width / 2);
+    this.centerY = this.drawY + (this.height / 2);
+    this.targetX = this.centerX;
+    this.targetY = this.centerY;
+    this.randomMoveTime = randomRange(4000, 10000);
+    this.speed = 1;
+    var that = this;
+    this.moveInterval = setInterval(function() {that.setTargetLocation();}, that.randomMoveTime);
+    this.isDead = false;
+}
+
+Enemy.prototype.update = function () {
+    this.centerX = this.drawX + (this.width / 2);
+    this.centerY = this.drawY + (this.height / 2);
+    this.checkDirection();
+};
+
+Enemy.prototype.draw = function () {
+    ctxEntities.drawImage(imgSprite, this.srcX, this.srcY, this.width, this.height, this.drawX, this.drawY, this.width, this.height);
+};
+
+Enemy.prototype.setTargetLocation = function () {
+    this.randomMoveTime = randomRange(4000, 10000);
+    var minX = this.centerX - 50,
+        maxX = this.centerX + 50,
+        minY = this.centerY - 50,
+        maxY = this.centerY + 50;
+    if (minX < 0) {
+        minX = 0;
+    }
+    if (maxX > canvasWidth) {
+        maxX = canvasWidth;
+    }
+    if (minY < 0) {
+        minY = 0;
+    }
+    if (maxY > canvasHeight) {
+        maxY = canvasHeight;
+    }
+    this.targetX = randomRange(minX, maxX);
+    this.targetY = randomRange(minY, maxY);
+};
+
+Enemy.prototype.checkDirection = function () {
+    if (this.centerX < this.targetX) {
+        this.drawX += this.speed;
+    } else if (this.centerX > this.targetX) {
+        this.drawX -= this.speed;
+    }
+    if (this.centerY < this.targetY) {
+        this.drawY += this.speed;
+    } else if (this.centerY > this.targetY) {
+        this.drawY -= this.speed;
+    }
+};
+
+Enemy.prototype.die = function () {
+    var soundEffect = new Audio("audio/dying.wav");
+    soundEffect.play();
+    clearInterval(this.moveInterval);
+    this.srcX = 185;
+    this.isDead = true;
+};
+
+
+function initEnemies() {
+    for (var i = 0; i < numEnemies; i++) {
+        enemies[enemies.length] = new Enemy();
+    }
+}
+
+function updateAllEnemies() {
+    for (var i = 0; i < enemies.length; i++) {
+        enemies[i].update();
+    }
+}
+
+function drawAllEnemies() {
+    for (var i = 0; i < enemies.length; i++) {
+        enemies[i].draw();
+    }
+}
+
+
+
 
 
 function checkKey(e, value) {
@@ -218,50 +428,9 @@ function outOfBounds(a, x, y) {
         newLeftX < treeLineLeft;
 }
 
-
-function Enemy () {
-    this.srcX = 140;
-    this.srcY = 600;
-    this.width = 45;
-    this.height = 54;
-    this.drawX = randomRange(0, canvasWidth - this.width);
-    this.drawY = randomRange(0, canvasHeight - this.height);
-    this.centerX = this.drawX + (this.width / 2 );
-    this.centerY = this.drawY + (this.height / 2 );
-    //this.targetX = this.centerX;
-    //this.targetY = this.centerY;
-    //this.randomMoveTime = randomRange(4000,10000);
-    this.speed = 1; //speed of the enemy
-    //var that = this;
-    //this.moveInterval = setInterval(function() {that.setTargetLocation();}, that.randomMoveTime);
-    this.isDead = false;
+function collision(a, b) {
+    return a.drawX <= b.drawX + b.width &&
+        a.drawX >= b.drawX &&
+        a.drawY <= b.drawY + b.height &&
+        a.drawY >= b.drawY;
 }
-
-Enemy.prototype.update = function () {
-    //this.checkDirection();
-    this.centerX = this.drawX + (this.width / 2 );
-    this.centerY = this.drawY + (this.height / 2 );
-}
-
-Enemy.prototype.draw = function () {
-    ctxEntities.drawImage(imgSprite, this.srcX, this.srcY, this.width, this.height, this.drawX, this.drawY, this.width, this.height)
-}
-
-function initEnemies () {
-    for (var i = 0; i < numEnemies; i++) {
-        enemies[enemies.length] = new Enemy();
-    }
-}
-
-function updateAllEnemies () {
-    for (var i = 0; i < enemies.length; i++) { 
-        enemies[i].update();
-    }
-}
-
-function drawAllEnemies () {
-    for (var i = 0; i < enemies.length; i++) { 
-        enemies[i].draw();
-    }
-}
-
